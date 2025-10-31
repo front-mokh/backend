d05386008d0ef332813a13e9c57557e58e19a45d
front-mokh <142145455+front-mokh@users.noreply.github.com>
2025-10-31 15:06:59 +0100

    feat(user): use type column with enum (brand|creator)\n\n- Add App\Enums\UserType (brand, creator)\n- Migration adds users.type (nullable, indexed)\n- User model casts/fillable updated to type\n- UserFactory sets type to brand|creator

---
+++ app/Enums/UserType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum UserType: string
+{
+    case BRAND = 'brand';
+    case CREATOR = 'creator';
+}
+
+
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Enums\UserType;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -29,6 +30,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'type' => fake()->randomElement([
+                UserType::BRAND->value,
+                UserType::CREATOR->value,
+            ]),
         ];
     }
 
@@ -37,7 +42,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
